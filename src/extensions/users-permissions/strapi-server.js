d0c308ab9389f62293a9ead621e7d6971ed355ae
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2022-12-08 14:29:11 -0600

    update services to show insuranceImgs field

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -23,6 +23,38 @@ module.exports = (plugin) => {
       policies: [],
     },
   });
+  //Enable insuranceImgs to populate on request
+  const sanitizeOutput = (user) => {
+    const {
+      password,
+      resetPasswordToken,
+      confirmationToken,
+      ...sanitizedUser
+    } = user; // be careful, you need to omit other private attributes yourself
+    return sanitizedUser;
+  };
+
+  plugin.controllers.user.me = async (ctx) => {
+    if (!ctx.state.user) {
+      return ctx.unauthorized();
+    }
+    const user = await strapi.entityService.findOne(
+      "plugin::users-permissions.user",
+      ctx.state.user.id,
+      { populate: ["role", "insuranceImgs"] }
+    );
+
+    ctx.body = sanitizeOutput(user);
+  };
+
+  plugin.controllers.user.find = async (ctx) => {
+    const users = await strapi.entityService.findMany(
+      "plugin::users-permissions.user",
+      { ...ctx.params, populate: ["role", "insuranceImgs"] }
+    );
+
+    ctx.body = users.map((user) => sanitizeOutput(user));
+  };
 
   return plugin;
 };
