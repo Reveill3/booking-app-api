fc2bd298d0899209ea138773f6fb8c75ff83a450
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2022-12-11 22:01:35 -0600

    booking summary functional and backend price calculate route

--- src/api/reservation/controllers/reservation.js
+++ src/api/reservation/controllers/reservation.js
@@ -1,9 +1,33 @@
-'use strict';
+"use strict";
 
 /**
  * reservation controller
  */
+const joi = require("joi");
+const getPriceSchema = joi.object({
+  location: joi.number().required(),
+  car: joi.number().required(),
+  totalDays: joi.number().required(),
+});
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController(
+  "api::reservation.reservation",
+  ({ strapi }) => ({
+    async getPrice(ctx) {
+      const { error } = getPriceSchema.validate(ctx.request.body);
+      if (error) {
+        return ctx.badRequest(error.message, null);
+      }
+      const price = await strapi
+        .service("api::reservation.reservation")
+        .getPrice(
+          ctx.request.body.location,
+          ctx.request.body.car,
+          ctx.request.body.totalDays
+        );
 
-module.exports = createCoreController('api::reservation.reservation');
+      return price;
+    },
+  })
+);
--- src/api/reservation/services/reservation.js
+++ src/api/reservation/services/reservation.js
@@ -1,9 +1,59 @@
-'use strict';
+"use strict";
 
 /**
  * reservation service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::reservation.reservation');
+module.exports = createCoreService(
+  "api::reservation.reservation",
+  ({ strapi }) => ({
+    async getPrice(location, car, totalDays) {
+      const query = {
+        filters: {
+          $or: [
+            {
+              locations: {
+                id: { $eq: location },
+              },
+            },
+            {
+              locations: {
+                name: { $eq: "all" },
+              },
+            },
+            {
+              cars: {
+                id: { $eq: car },
+              },
+            },
+          ],
+        },
+      };
+      const addOns = await strapi.entityService.findMany(
+        "api::add-on.add-on",
+        query
+      );
+
+      const carLookup = await strapi.entityService.findOne(
+        "api::car.car",
+        car,
+        {
+          fields: ["dailyRate"],
+        }
+      );
+
+      const subTotal = carLookup.dailyRate * totalDays;
+
+      const feeTotal = addOns.reduce((acc, addOn) => {
+        if (addOn.type === "percentage") {
+          return acc + subTotal * addOn.amount;
+        }
+        return acc + addOn.amount;
+      }, 0);
+
+      return subTotal + feeTotal;
+    },
+  })
+);
